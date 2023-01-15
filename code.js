7b2ab8d9158fe06bab62b5ce379b78a3909baa9a
altair9595 <mhdfadialtair@gmail.com>
2023-01-16 00:13:48 +0100

    first commit

---
+++ code.js
@@ -0,0 +1,42 @@
+// let name = 'fadi'; age = 20;
+// console.log(name)
+// console.log(age)
+
+// let married = false;
+// let Graduated = true;
+// console.log(married)
+
+// let value = 6 > 5;
+// console.log(value)
+// // 
+// let age =prompt (' how old are you');
+// console.log(age)
+
+// let married = confirm('are you married');
+// console.log(married)
+
+// let age = prompt ('Enter your Age:');
+// if(age>20){
+//     console.log('ok')
+// }else{
+//     console.log('issue')
+// }
+// let name = 'fadi'
+// let password = 12345
+// let user_input = prompt ('Enter username')
+// let password_input= prompt('Enter password')
+// if(user_input==username){
+//     console.log('login succeded')
+
+// }else{
+//     console.log('login faild')
+// }
+
+let grade = prompt(' Enter your Grade')
+if (grade > 90){
+    console.log('A+')
+}else if (grade > 80){
+    console.log('B+')
+}else if (grade > 70){
+    console.log('C+')
+}
